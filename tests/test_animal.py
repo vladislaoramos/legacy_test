3b2bf7d80b80499ed916e279d38de3040553e457
Vladislav <vladislaoramos@gmail.com>
2021-11-01 21:24:56 +0300

    Update tests

--- tests/test_animal.py
+++ tests/test_animal.py
@@ -2,38 +2,53 @@ import unittest
 from animals import Animal
 
 
+INIT_PARAMS = {
+    "name": "Animal",
+    "energy": 100,
+    "energy_rate": {
+        "fly": 5,
+        "run": 10,
+        "swim": 15
+    }
+}
+
+
 class TestAnimal(unittest.TestCase):
-    @staticmethod
-    def init_animal():
-        return Animal("Animal", energy_rate={"fly": 5, "run": 10, "swim": 15})
+    def setUp(self):
+        self.animal = Animal(
+            name="Animal",
+            energy_rate={
+                "fly": 5,
+                "run": 10,
+                "swim": 15
+            }
+        )
+        self.param_animal = Animal(init_parameters=INIT_PARAMS)
 
     def test_speak(self):
-        animal = TestAnimal.init_animal()
-        self.assertTrue(animal.say())
-        self.assertEqual(animal.last_speech, "Hello, I'm an animal and my name is Animal.")
+        self.assertTrue(self.animal.say())
+        self.assertEqual(self.animal.last_speech, "Hello, I'm an animal and my name is Animal.")
 
     def test_create(self):
-        animal = TestAnimal.init_animal()
-        self.assertTrue(isinstance(animal, Animal))
+        self.assertTrue(isinstance(self.animal, Animal))
 
     def test_animal_name(self):
-        animal = TestAnimal.init_animal()
-        self.assertEqual(animal.name, "Animal")
+        self.assertEqual(self.animal.name, "Animal")
 
     def test_animal_energy(self):
-        animal = TestAnimal.init_animal()
-        self.assertEqual(animal.energy, 100)
-        self.assertNotEqual(animal.energy, 101)
-        self.assertNotEqual(animal.energy, 99)
+        self.assertEqual(self.animal.energy, 100)
+        self.assertNotEqual(self.animal.energy, 101)
+        self.assertNotEqual(self.animal.energy, 99)
 
     def test_unknown_action(self):
-        animal = TestAnimal.init_animal()
-        self.assertFalse(animal.do_something(action="play"))
-        self.assertFalse(animal.do_something(action="jump"))
-        self.assertFalse(animal.do_something(action="think"))
+        self.assertFalse(self.animal.do_something(action="play"))
+        self.assertFalse(self.animal.do_something(action="jump"))
+        self.assertFalse(self.animal.do_something(action="think"))
 
     def test_init_params(self):
-        pass
+        self.assertEqual(self.param_animal.name, INIT_PARAMS.get("name", "Animal"))
+        self.assertEqual(self.param_animal.energy, INIT_PARAMS.get("energy", 100))
+        self.assertEqual(self.param_animal.energy_rate, INIT_PARAMS.get("energy_rate", {}))
 
 
 if __name__ == "__main__":
--- tests/test_animals.py
+++ tests/test_animals.py
@@ -2,95 +2,149 @@ import unittest
 from animals import Cat, Dog, Duck, Tiger, Fish, FlyingFish
 
 
-test_cat = Cat()
-test_dog = Dog()
-test_fish = Fish()
-test_flying_fish = FlyingFish()
-test_tiger = Tiger()
-test_duck = Duck()
+class WithNameTestCase(unittest.TestCase):
+    def setUp(self):
+        self.with_name = [
+            (Cat("Cat"), "Cat"), (Cat("Barsik"), "Barsik"),
+            (Dog("Bobik"), "Bobik"), (Dog("Dog"), "Dog"),
+            (Fish("Fish"), "Fish"), (Fish("Nemo"), "Nemo"),
+            (Tiger("Tiger"), "Tiger"), (Tiger("Alex"), "Alex"),
+            (FlyingFish("FlyingFish"), "FlyingFish"), (FlyingFish("Goldfish"), "Goldfish"),
+            (Duck("Duck"), "Duck"), (Duck("Donald"), "Donald")
+        ]
 
-name_test = [
-    (test_cat, "Cat"), (test_dog, "Dog"),
-    (test_fish, "Fish"), (test_tiger, "Tiger"),
-    (test_flying_fish, "FlyingFish"), (test_duck, "Duck")
-]
-
-energy_test = [
-    (Cat(), 100), (Dog(), 100),
-    (Fish(), 100), (Tiger(), 100),
-    (FlyingFish(), 100), (Duck(), 100)
-]
-
-actions_single_test = [
-    (test_cat, "run", True), (test_cat, "swim", False), (test_cat, "fly", False),
-
-    (test_dog, "run", True), (test_dog, "swim", True), (test_dog, "fly", False),
-
-    (test_fish, "run", False), (test_fish, "swim", True), (test_fish, "fly", False),
-
-    (test_flying_fish, "run", False), (test_flying_fish, "fly", True), (test_flying_fish, "swim", True),
-
-    (test_tiger, "run", True), (test_tiger, "swim", True), (test_tiger, "fly", False),
-
-    (test_duck, "run", False), (test_duck, "swim", True), (test_duck, "fly", True)
-]
-
-actions_energy_test = [
-    (test_cat, "run", 5), (test_dog, "run", 10),
-    (test_fish, "run", 0), (test_flying_fish, "run", 0),
-    (test_tiger, "run", 20), (test_duck, "run", 0),
-    (test_cat, "swim", 0), (test_dog, "swim", 30),
-    (test_fish, "swim", 5), (test_flying_fish, "swim", 5),
-    (test_tiger, "swim", 40), (test_duck, "swim", 10),
-    (test_cat, "fly", 0), (test_dog, "fly", 0),
-    (test_fish, "fly", 0), (test_flying_fish, "fly", 20),
-    (test_tiger, "fly", 0), (test_duck, "fly", 30),
-]
-
-new_tiger = Tiger()
-new_dog = Dog()
-new_duck = Duck()
-
-several_actions_test = [
-    (new_tiger, "run", True),
-    (new_tiger, "run", True),
-    (new_tiger, "run", True),
-    (new_tiger, "run", True),
-    (new_tiger, "run", True),
-    (new_tiger, "run", False),
-
-    (new_dog, "swim", True),
-    (new_dog, "swim", True),
-    (new_dog, "swim", True),
-    (new_dog, "swim", False),
-
-    (new_duck, "fly", True),
-    (new_duck, "fly", True),
-    (new_duck, "fly", True),
-    (new_duck, "fly", False),
-]
-
-
-class TestAnimals(unittest.TestCase):
     def test_name(self):
-        for test, expected in name_test:
-            self.assertEqual(test.name, expected)
+        for animal, expected in self.with_name:
+            self.assertEqual(animal.name, expected)
+
+
+class WithoutNameTestCase(unittest.TestCase):
+    def setUp(self):
+        self.without_name = [
+            (Cat(), "Cat"), (Dog(), "Dog"),
+            (Fish(), "Fish"), (Tiger(), "Tiger"),
+            (FlyingFish(), "FlyingFish"), (Duck(), "Duck")
+        ]
+
+    def test_name(self):
+        for animal, expected in self.without_name:
+            self.assertEqual(animal.name, expected)
+
+
+class EnergyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.energy_test = [
+            (Cat(), 100, 95), (Dog(), 100, 60),
+            (Fish(), 100, 95), (Tiger(), 100, 40),
+            (FlyingFish(), 100, 75), (Duck(), 100, 60)
+        ]
 
     def test_energy(self):
-        for test, expected in energy_test:
-            self.assertEqual(test.energy, expected)
+        for animal, start, remaining in self.energy_test:
+            self.assertEqual(animal.energy, start)
+            for action in "run", "swim", "fly":
+                animal.do_something(action=action)
+            self.assertEqual(animal.energy, remaining)
 
-    def test_single_actions(self):
-        for animal, action, expected in actions_single_test:
-            self.assertEqual(animal.do_something(action=action), expected)
 
-    def test_action_rate(self):
-        for animal, action, expected_rate in actions_energy_test:
-            self.assertEqual(animal.energy_rate.get(action, 0), expected_rate)
+class SingleActionTestCase(unittest.TestCase):
+    def test_ability(self):
+        self.assertTrue(Cat().run())
 
-    def test_several_actions(self):
-        for animal, action, expected_ans in several_actions_test:
-            self.assertEqual(animal.do_something(action=action), expected_ans)
+        self.assertTrue(Dog().run())
+        self.assertTrue(Dog().swim())
+
+        self.assertTrue(Fish().swim())
+
+        self.assertTrue(FlyingFish().swim())
+        self.assertTrue(FlyingFish().fly())
+
+        self.assertTrue(Tiger().run())
+        self.assertTrue(Tiger().swim())
+
+        self.assertTrue(Duck().swim())
+        self.assertTrue(Duck().fly())
+
+    def test_inability(self):
+        self.assertFalse(Cat().swim())
+        self.assertFalse(Cat().fly())
+
+        self.assertFalse(Dog().fly())
+
+        self.assertFalse(Fish().run())
+        self.assertFalse(Fish().fly())
+
+        self.assertFalse(FlyingFish().run())
+
+        self.assertFalse(Tiger().fly())
+
+        self.assertFalse(Duck().run())
+
+
+class SeveralActionTestCase(unittest.TestCase):
+    def test_cat(self):
+        cat = Cat()
+        for _ in range(20):
+            self.assertTrue(cat.run())
+        self.assertFalse(cat.run())
+
+    def test_dog(self):
+        dog = Dog()
+        self.assertTrue(dog.run())
+        self.assertTrue(dog.swim())
+        self.assertTrue(dog.swim())
+        self.assertTrue(dog.swim())
+        self.assertFalse(dog.run())
+
+    def test_tiger(self):
+        tiger = Tiger()
+        self.assertTrue(tiger.swim())
+        self.assertTrue(tiger.swim())
+        self.assertTrue(tiger.run())
+        self.assertFalse(tiger.run())
+        self.assertFalse(tiger.swim())
+
+    def test_fish(self):
+        fish = Fish()
+        for _ in range(20):
+            self.assertTrue(fish.swim())
+        self.assertFalse(fish.swim())
+
+    def test_flying_fish(self):
+        fish = FlyingFish()
+        for _ in range(4):
+            self.assertTrue(fish.swim())
+            self.assertTrue(fish.fly())
+        self.assertFalse(fish.swim())
+        self.assertFalse(fish.fly())
+
+    def test_duck(self):
+        duck = Duck()
+        self.assertTrue(duck.fly())
+        self.assertTrue(duck.fly())
+        self.assertTrue(duck.fly())
+        self.assertTrue(duck.swim())
+        self.assertFalse(duck.swim())
+
+
+class EnergyRateTestCase(unittest.TestCase):
+    def setUp(self):
+        self.rates = [
+            (Cat(), 5),
+            (Dog(), 40),
+            (Tiger(), 60),
+            (Fish(), 5),
+            (FlyingFish(), 25),
+            (Duck(), 40)
+        ]
+
+    def test_energy_rate(self):
+        for animal, expected in self.rates:
+            start = animal.get_energy()
+            for action in "run", "swim", "fly":
+                animal.do_something(action=action)
+            remaining = animal.get_energy()
+            self.assertEqual(start - remaining, expected)
 
 
 if __name__ == "__main__":
